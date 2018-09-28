270b8ed5692cfc2316d8a5127349492f7dd2dcdb
AlfieriChou <alfierichou@gmail.com>
2018-09-28 12:52:29 +0800

    update require dir

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,2 +1,10 @@
-const requireDirectory = require('require-directory')
-module.exports = requireDirectory(module)
+const path = require('path')
+const fs = require('fs')
+const appRoot = require('app-root-path')
+const dir = path.resolve(`${appRoot}/app/controller/`)
+fs.readdirSync(dir).forEach(file => {
+  if (file.match(/\.js$/) !== null && file !== 'index.js') {
+    const name = file.replace('.js', '')
+    exports[name] = require('./' + file)
+  }
+})
--- app/service/index.js
+++ app/service/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const appRoot = require('app-root-path')
-const dir = path.resolve(`${appRoot}/app/swagger/`)
+const dir = path.resolve(`${appRoot}/app/service/`)
 fs.readdirSync(dir).forEach(file => {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
     const name = file.replace('.js', '')
