47976305452920e529dcc6ad1cd2a24a23ff5e7b
AlfieriChou <alfierichou@gmail.com>
2018-09-30 13:44:32 +0800

    utf-8

--- migrations/cat.js
+++ migrations/cat.js
@@ -11,6 +11,8 @@ module.exports = (queryInterface, Sequelize) => {
             created_at: { type: Sequelize.DATE, allowNull: false },
             updated_at: { type: Sequelize.DATE, allowNull: false },
             deleted_at: { type: Sequelize.DATE }
+          }, {
+            charset: 'utf8'
           }
         )
       }
