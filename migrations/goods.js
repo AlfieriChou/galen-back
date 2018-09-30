ef6cd0e16a2e3a982996eb63887623aa343370f1
AlfieriChou <alfierichou@gmail.com>
2018-09-30 17:24:09 +0800

    add goods migration

---
+++ migrations/goods.js
@@ -0,0 +1,16 @@
+module.exports = (Sequelize) => {
+  return [
+    {
+      opt: 'create',
+      table: 'goods',
+      column: {
+        id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV1 },
+        name: { type: Sequelize.STRING },
+        description: { type: Sequelize.TEXT },
+        created_at: { type: Sequelize.DATE, allowNull: false },
+        updated_at: { type: Sequelize.DATE, allowNull: false },
+        deleted_at: { type: Sequelize.DATE }
+      }
+    }
+  ]
+}
