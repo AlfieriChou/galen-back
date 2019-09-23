ea8d9bcfc0fa3ca7ec2e8216613685718fd48157
AlfieriChou <alfierichou@gmail.com>
2019-09-23 09:50:46 +0800

    fix: eslint warning

--- app/model/user.js
+++ app/model/user.js
@@ -1,4 +1,4 @@
-
+// eslint-disable-next-line func-names
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define('User', {
     phone: { type: DataTypes.STRING, comment: '手机号' },
